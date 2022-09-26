c53b7b0224c1c6349aa139846e2651885ddb60c8
Twan <t.peters01@student.alfa-college.nl>
2022-09-26 10:51:29 +0200

    small changes and buggfixes
    1.0.2

--- webshop/resources/views/categories.blade.php
+++ webshop/resources/views/categories.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Shop Homepage - Start Bootstrap Template</title>
+    <title>Game Webshop</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap icons-->
@@ -20,21 +20,22 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#!">Start Bootstrap</a>
+            <a class="navbar-brand" href="#!">Gaming Shop</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="http://127.0.0.1:8000">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Over ons</a></li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Winkelen</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#!">All Products</a></li>
+                            <li><a class="dropdown-item" href="#!">Producten</a></li>
                             <li>
                                 <hr class="dropdown-divider" />
                             </li>
-                            <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                            <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            <li><a class="dropdown-item" href="#!">Categorie 1</a></li>
+                            <li><a class="dropdown-item" href="#!">Categorie 2</a></li>
+                            <li><a class="dropdown-item" href="#!">Categorie 3</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -53,14 +54,14 @@
         <div class="container px-4 px-lg-5 my-5">
             <div class="text-center text-white">
                 <h1 class="display-4 fw-bolder">Shop in style</h1>
-                <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                <p class="lead fw-normal text-white-50 mb-0">With this shop homepage template</p>
             </div>
         </div>
     </header>
     <!-- Section-->
     <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
-            <div class="row' style=" background-color: white; padding: 0px;">
+            <div class="row" style=" background-color: white; padding: 0px;">
 
 
                 <div class="container">
@@ -70,7 +71,8 @@
 
                         <div class="col-sm-4">
                             <a style="text-decoration: none;" href="https://www.bol.com/">
-                                <div style=" border:black; border-style:solid; border-width:thin; margin-bottom:50px; background-color: orange; padding: 50px; text-align: center; text-decoration: none; ">{{($category->name)}}</div>
+                                <div style=" border:black; border-style:solid; border-width:thin; margin-bottom:5px; background-color: orange; padding: 50px; text-align: center; text-decoration: none; ">{{($category->name)}}</div>
+                                <div class="text-center" style="margin-bottom:50px;"><a class="btn btn-outline-dark mt-auto" href="#">Toevoegen aan winkelwagen</a></div>
                             </a>
                         </div>
 
@@ -83,7 +85,7 @@
 
                 <!-- Product actions-->
                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+
                 </div>
             </div>
         </div>
@@ -93,7 +95,7 @@
     <!-- Footer-->
     <footer class="py-5 bg-dark">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p>
+            <p class="m-0 text-center text-white">Copyright 2022 &copy; Webshop Gaming. All rights reserved</p>
         </div>
     </footer>
     <!-- Bootstrap core JS-->
